c98a6b22702a8d8d872921fdedc9b831e135dadd
Trxae <80694250+Trxae@users.noreply.github.com>
2024-04-09 01:00:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ twitch_miner = TwitchChannelPointsMiner(
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
         ),
         discord=Discord(
-            webhook_api="https://discord.com/api/webhooks/0123456789/0a1B2c3D4e5F6g7H8i9J",  # Discord Webhook URL
+            webhook_api="https://discord.com/api/webhooks/1227012095996858378/TA_tOru_uPvR8ub54G5hC7_TA5zNdWrIbGBakYOBeDe3xEd3F6lp9tnNW7xN1zgyDqC9",  # Discord Webhook URL
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.BET_LOSE, Events.CHAT_MENTION],                                  # Only these events will be sent to the chat
         ),
